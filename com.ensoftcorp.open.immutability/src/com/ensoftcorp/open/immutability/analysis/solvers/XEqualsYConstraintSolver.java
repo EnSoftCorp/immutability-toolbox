0104dc61b2e0d9e98e702873d8a42ffb18ff738d
benjholla <benjholla@gmail.com>
2016-12-01 14:10:54 -0600

    more efficient equality constraint solver

--- com.ensoftcorp.open.immutability/src/com/ensoftcorp/open/immutability/analysis/solvers/XEqualsYConstraintSolver.java
+++ com.ensoftcorp.open.immutability/src/com/ensoftcorp/open/immutability/analysis/solvers/XEqualsYConstraintSolver.java
@@ -1,7 +1,6 @@
 package com.ensoftcorp.open.immutability.analysis.solvers;
 
 import static com.ensoftcorp.open.immutability.analysis.AnalysisUtilities.getTypes;
-import static com.ensoftcorp.open.immutability.analysis.AnalysisUtilities.removeTypes;
 
 import java.util.EnumSet;
 import java.util.Set;
@@ -13,20 +12,22 @@ import com.ensoftcorp.open.immutability.analysis.ImmutabilityTypes;
 public class XEqualsYConstraintSolver {
 
 	public static boolean satisfty(Node x, ImmutabilityTypes y){
-		
 		Set<ImmutabilityTypes> xTypes = getTypes(x);
 		if (xTypes.isEmpty()) {
 			Log.warning("x [" + x.address().toAddressString() + "] is untyped, constraint 'qx equals y' cannot be satisfied", new IllegalArgumentException());
 			return false;
 		}
-
-		Set<ImmutabilityTypes> xTypesToRemove = EnumSet.noneOf(ImmutabilityTypes.class);
-		for(ImmutabilityTypes xType : xTypes){
-			if(!xType.equals(y)){
-				xTypesToRemove.add(xType);
-			}
+		
+		switch(y){
+			case MUTABLE:
+				return xTypes.removeAll(EnumSet.of(ImmutabilityTypes.READONLY, ImmutabilityTypes.POLYREAD));
+			case POLYREAD:
+				return xTypes.removeAll(EnumSet.of(ImmutabilityTypes.READONLY, ImmutabilityTypes.MUTABLE));
+			case READONLY:
+				return xTypes.removeAll(EnumSet.of(ImmutabilityTypes.POLYREAD, ImmutabilityTypes.MUTABLE));
+			default:
+				throw new IllegalArgumentException("Unexpected immutability type!");
 		}
-		return removeTypes(x, xTypesToRemove);
 	}
 	
 //	public static boolean satisify(Node x, Node y) {
